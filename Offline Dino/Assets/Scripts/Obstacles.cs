cd53eaec83af56c44bd7893741cdaaa58c1fc16e
Ahmet Türkmen <108531432+turkmenahmet@users.noreply.github.com>
2023-01-29 12:33:51 +0300

    Obstacles

---
+++ Offline Dino/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    private float leftEdge;
+    private void Start(){
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 2f;
+    }
+    private void Update(){
+        transform.position += Vector3.left * GameManager.Instance.gameSpeed * Time.deltaTime;
+
+        if(transform.position.x < leftEdge){
+            Destroy(gameObject);
+        }
+    }
+}
